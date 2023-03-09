44304c865fd480021d6334fe0b38a7736494e3fe
bastien.siguier <bastien.siguier@gmail.com>
2023-03-09 17:18:09 +0100

    trying to add new ontology

--- addOn/middleware/services/core.service.js
+++ addOn/middleware/services/core.service.js
@@ -3,12 +3,14 @@ const urlJoin = require("url-join");
 const { CoreService } = require('@semapps/core');
 const CONFIG = require('../config/config');
 const containers = require('../config/containers');
+const ApiGatewayService = require('moleculer-web');
 
 module.exports = {
   mixins: [CoreService],
   settings: {
     baseUrl: CONFIG.HOME_URL,
     baseDir: path.resolve(__dirname, '..'),
+    jsonContext: urlJoin('https://data.essai-possible.data-players.com/','context.json'),
     triplestore: {
       url: CONFIG.SPARQL_ENDPOINT,
       user: CONFIG.JENA_USER,
@@ -18,6 +20,30 @@ module.exports = {
     containers,
     api: {
       port: CONFIG.PORT,
+      routes: [
+        {
+          path: '/ontology',
+          use: [
+            ApiGatewayService.serveStatic('./public/ontology.ttl', {
+              setHeaders: res => {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'text/turtle; charset=utf-8');
+              }
+            })
+          ]
+        },
+        {
+           path: '/context.json',
+           use: [
+             ApiGatewayService.serveStatic('./public/context.json', {
+               setHeaders: res => {
+                 res.setHeader('Access-Control-Allow-Origin', '*');
+                 res.setHeader('Content-Type', 'application/json; charset=utf-8');
+               }
+             })
+           ]
+       }
+      ]
     },
     ldp: {
       preferredViewForResource: async (resourceUri, containerPreferredView) => {
