c494d8d2c6bf3e4d6a3b04025c457c4fa074717c
bastien.siguier <bastien.siguier@gmail.com>
2023-03-14 13:09:51 +0100

    adding ontology

--- addOn/middleware/services/core.service.js
+++ addOn/middleware/services/core.service.js
@@ -3,12 +3,14 @@ const urlJoin = require("url-join");
 const { CoreService } = require('@semapps/core');
 const CONFIG = require('../config/config');
 const containers = require('../config/containers');
+const ApiGatewayService = require('moleculer-web');
 
 module.exports = {
   mixins: [CoreService],
   settings: {
     baseUrl: CONFIG.HOME_URL,
     baseDir: path.resolve(__dirname, '..'),
+    jsonContext: urlJoin('https://data.petr-peps.data-players.com/context.json'),
     triplestore: {
       url: CONFIG.SPARQL_ENDPOINT,
       user: CONFIG.JENA_USER,
@@ -18,13 +20,32 @@ module.exports = {
     containers,
     api: {
       port: CONFIG.PORT,
+      routes: [
+        {
+          path: '/ontology',
+          use: [
+            ApiGatewayService.serveStatic('./public/ontology.ttl', {
+              setHeaders: res => {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Content-Type', 'text/turtle; charset=utf-8');
+              }
+            })
+          ]
+        },
+        {
+           path: '/context.json',
+           use: [
+             ApiGatewayService.serveStatic('./public/context.json', {
+               setHeaders: res => {
+                 res.setHeader('Access-Control-Allow-Origin', '*');
+                 res.setHeader('Content-Type', 'application/json; charset=utf-8');
+               }
+             })
+           ]
+       }
+      ]
     },
-    ldp: {
-      preferredViewForResource: async (resourceUri, containerPreferredView) => {
-        if (!containerPreferredView) return resourceUri;
-        return urlJoin(CONFIG.FRONT_URL, containerPreferredView, encodeURIComponent(resourceUri), 'show')
-      }
-    },
+    ldp: false,
     void: {
       title: CONFIG.INSTANCE_NAME,
       description: CONFIG.INSTANCE_DESCRIPTION
