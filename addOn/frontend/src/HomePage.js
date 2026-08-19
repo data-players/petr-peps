2ffd361e701391fbb49f1011d04215b8fa29d3d1
simon louvet <simon.louvet.pro@gmail.com>
2026-08-19 17:01:43 +0200

    remove diagnostic patch and disable HomePage redirect to test token loss

--- addOn/frontend/src/HomePage.js
+++ addOn/frontend/src/HomePage.js
@@ -2,8 +2,9 @@ import React from 'react';
 import OrganizationList from './resources/Agent/Actor/Organization/OrganizationList'
 
 const HomePage = () => {
-  //To do change this
-  window.location.replace('/Organization');
+  // To do change this
+  // Redirection désactivée temporairement pour tester l'hypothèse de la perte de token
+  // window.location.replace('/Organization');
 
   return (
     <></>
