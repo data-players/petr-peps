22ea0349c8b744b7bc26ae2a1bc6cadff67e8d81
simon louvet <simon.louvet.pro@gmail.com>
2026-08-19 17:23:12 +0200

    make HomePage render OrganizationList directly instead of redirecting

--- addOn/frontend/src/HomePage.js
+++ addOn/frontend/src/HomePage.js
@@ -2,13 +2,7 @@ import React from 'react';
 import OrganizationList from './resources/Agent/Actor/Organization/OrganizationList'
 
 const HomePage = () => {
-  // To do change this
-  // Redirection désactivée temporairement pour tester l'hypothèse de la perte de token
-  // window.location.replace('/Organization');
-
-  return (
-    <></>
-  );
+  return <OrganizationList />;
 };
 
 export default HomePage;
